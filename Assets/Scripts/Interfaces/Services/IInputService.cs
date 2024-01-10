a96100d28fa093b6f5f0279c93e092c972657750
Egor Volobuev <Egor845@yandex.ru>
2024-01-10 22:05:57 +0700

    [Logic][Dev] - The service responsible for player input has been implemented

---
+++ Assets/Scripts/Interfaces/Services/IInputService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Interfaces.Services
+{
+    public interface IInputService
+    {
+        IObservable<Vector2> Direction { get; }
+        IObservable<Unit> Interact { get; }
+    }
+}
---
+++ Assets/Scripts/Services/InputService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Interfaces.Services;
+using UniRx;
+using UnityEngine;
+
+namespace Services
+{
+    public class InputService : IInputService
+    {
+        public IObservable<Vector2> Direction => Observable.EveryUpdate()
+            .Select(_ => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+
+        public IObservable<Unit> Interact =>
+            Observable.EveryUpdate().Where(_ => Input.GetKeyDown(KeyCode.E)).AsUnitObservable();
+    }
+}
