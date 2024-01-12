6437f94bff7e162c99efcc126e20c3bff0382474
Egor Volobuev <Egor845@yandex.ru>
2024-01-12 21:23:21 +0700

    [Logic][Fix] - Fixed indentation in code

--- Assets/Scripts/Core/ViewModels/ViewModel.cs
+++ Assets/Scripts/Core/ViewModels/ViewModel.cs
@@ -8,9 +8,9 @@ namespace Core.ViewModels
     public abstract class ViewModel<TModel> : IViewModel<TModel> where TModel : IModel
     {
         public TModel Model { get; private set; }
-    
+
         protected ICollection<IDisposable> Disposables { get; } = new List<IDisposable>();
-    
+
         private readonly IDataService _dataService;
 
         protected ViewModel(IDataService dataService)
