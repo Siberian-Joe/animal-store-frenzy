a103baa62937edfb60a710c2dc38fafbf1b2c3e2
Egor Volobuev <volobuev@tiltshift.xyz>
2024-01-28 03:57:58 +0700

    [Logic][Dev] - Added a basic implementation of a moving entity

---
+++ Assets/Scripts/Core/ViewModels/MovementStrategies/InputMovementStrategy.cs
@@ -0,0 +1,39 @@
+﻿using Interfaces.Core;
+using UniRx;
+using UnityEngine;
+
+namespace Core.ViewModels.MovementStrategies
+{
+    public class PlayerInputMovementStrategy : IMovementStrategy
+    {
+        private readonly Transform _transform;
+        private readonly CompositeDisposable _disposable = new();
+
+        private float _speed;
+
+        public PlayerInputMovementStrategy(Transform transform, IReadOnlyReactiveProperty<float> speed)
+        {
+            _transform = transform;
+
+            speed
+                .Subscribe(SetSpeed)
+                .AddTo(_disposable);
+        }
+
+        public void Move(Vector2 direction)
+        {
+            var clampedDirection = Vector2.ClampMagnitude(direction, 1);
+            _transform.Translate(clampedDirection * _speed * Time.fixedDeltaTime);
+        }
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
+        }
+
+        private void SetSpeed(float speed)
+        {
+            _speed = speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Interfaces/Core/IMovementStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interfaces.Core
+{
+    public interface IMovementStrategy : IDisposable
+    {
+        void Move(Vector2 direction);
+    }
+}
