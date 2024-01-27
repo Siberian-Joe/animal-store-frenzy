562368c765c7e9331d3c60a4116b3943fc7f9651
Egor Volobuev <Egor845@yandex.ru>
2024-01-28 03:54:43 +0700

    [Logic][Dev] - Added base entity implementation

---
+++ Assets/Scripts/Core/ViewModels/EntityViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Core.Models;
+using Interfaces.Core;
+using Interfaces.Services.DataServices;
+using UniRx;
+using UnityEngine;
+
+namespace Core.ViewModels
+{
+    public abstract class EntityViewModel<TModel> : ViewModel<TModel>, IEntityViewModel where TModel : EntityModel
+    {
+        public IReadOnlyReactiveProperty<Transform> Transform => Model.Transform;
+
+        protected EntityViewModel(IDataService dataService) : base(dataService)
+        {
+            Observable.EveryUpdate().Subscribe(_ => Update()).AddTo(Disposable);
+            Observable.EveryFixedUpdate().Subscribe(_ => FixedUpdate()).AddTo(Disposable);
+        }
+
+        protected virtual void Update()
+        {
+        }
+        
+        protected virtual void FixedUpdate()
+        {
+        }
+
+        public void SetTransform(Transform transform)
+        {
+            Model.Transform.Value = transform;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Views/EntityView.cs
@@ -0,0 +1,14 @@
+﻿using Interfaces.Core;
+
+namespace Core.Views
+{
+    public abstract class EntityView<TViewModel> : View<TViewModel> where TViewModel : IEntityViewModel
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            ViewModel.SetTransform(transform);
+        }
+    }
+}
