94f803dc55d9632888e8c9b1a363af826026d1dd
Egor Volobuev <Egor845@yandex.ru>
2024-02-01 01:27:55 +0700

    [Logic][Dev] - Implemented logging service

---
+++ Assets/Scripts/Interfaces/Services/ILoggingService.cs
@@ -0,0 +1,8 @@
+﻿namespace Interfaces.Services
+{
+    public interface ILoggingService {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+    }
+}
