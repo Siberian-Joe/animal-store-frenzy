2606a6b35a30e76d93abdaa9ae22afd6b0607dd4
Egor Volobuev <Egor845@yandex.ru>
2024-04-10 23:40:32 +0700

    [Logic][Dev] - Added interactive and moving entities

---
+++ Assets/Scripts/Core/Models/MovableAndInteractableEntityModel.cs
@@ -0,0 +1,10 @@
+﻿using Core.Enums;
+using UniRx;
+
+namespace Core.Models
+{
+    public class MovableAndInteractableEntityModel : MovableEntityModel
+    {
+        public ReactiveProperty<InteractableEntityType> InteractableEntityType { get; } = new();
+    }
+}
---
+++ Assets/Scripts/Core/Views/MovableAndInteractableEntityView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Interfaces.Core;
+using Interfaces.Interactions;
+
+namespace Core.Views
+{
+    public abstract class MovableAndInteractableEntityView<TViewModel> : MovableEntityView<TViewModel>, IInteractable
+        where TViewModel : IMovableEntityViewModel, IInteractableEntityViewModel
+    {
+        public void Interact<TInteraction>(Action<TInteraction> action = null) where TInteraction : IInteraction
+        {
+            ViewModel.Interact(action);
+        }
+    }
+}
