79535ea44561e3b75e9609e16cbedd040c01c021
Egor Volobuev <volobuev@tiltshift.xyz>
2024-10-18 01:25:48 +0700

    [Logic][Dev] - Added a basic implementation for the interactive object locator

---
+++ Assets/Scripts/Services/IInteractiveObjectLocationService.cs
@@ -0,0 +1,14 @@
+﻿namespace Services
+{
+    public interface IObjectRegistrationService
+    {
+        void Register(IInteractiveObject interactiveObject);
+        void Unregister(IInteractiveObject interactiveObject);
+    }
+
+    public interface IObjectQueryService
+    {
+        bool Has<T>() where T : IInteractiveObject;
+        bool TryFindNearest<T>(IInteractiveObject from, out T nearest) where T : IInteractiveObject;
+    }
+}
---
+++ Assets/Scripts/Services/InteractiveObjectLocationService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public class InteractiveObjectLocationService : IObjectRegistrationService, IObjectQueryService
+    {
+        private readonly Dictionary<Type, List<IInteractiveObject>> _locations = new();
+        
+        public void Register(IInteractiveObject interactiveObject)
+        {
+            var type = interactiveObject.GetType();
+            if (_locations.ContainsKey(type) == false)
+            {
+                _locations[type] = new List<IInteractiveObject>();
+            }
+
+            _locations[type].Add(interactiveObject);
+        }
+        
+        public void Unregister(IInteractiveObject interactiveObject)
+        {
+            var type = interactiveObject.GetType();
+            if (_locations.ContainsKey(type))
+            {
+                _locations[type].Remove(interactiveObject);
+                if (_locations[type].Count == 0)
+                {
+                    _locations.Remove(type);
+                }
+            }
+        }
+
+        public bool Has<T>() where T : IInteractiveObject
+        {
+            return _locations.ContainsKey(typeof(T));
+        }
+
+        public bool TryFindNearest<T>(IInteractiveObject from, out T nearest) where T : IInteractiveObject
+        {
+            nearest = default;
+            if (_locations.TryGetValue(typeof(T), out var interactiveObjects) == false)
+            {
+                return false;
+            }
+
+            var nearestDistance = float.MaxValue;
+            foreach (var interactiveObject in interactiveObjects)
+            {
+                var distance = (from.Position - interactiveObject.Position).sqrMagnitude;
+                if (distance < nearestDistance)
+                {
+                    nearest = (T)interactiveObject;
+                    nearestDistance = distance;
+                }
+            }
+
+            return nearest != null;
+        }
+    }
+}
